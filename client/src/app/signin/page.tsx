5b7a7e5372c46b42c8e31e110043d4b537f0fb20
Yonas Rezaee <30201953+rez433@users.noreply.github.com>
2024-10-02 09:02:30 +0200

    Update page.tsx

--- client/src/app/signin/page.tsx
+++ client/src/app/signin/page.tsx
@@ -7,37 +7,49 @@ import { useAuth } from '../AuthContext'
 export default function SignIn() {
   const [userEmail, setUserEmail] = useState('')
   const [password, setPassword] = useState('')
+	const [isLoading, setIsLoading] = useState(false)
   const router = useRouter()
   const { login } = useAuth()
 
 	const baseApiUrl: string = process.env.NEXT_PUBLIC_API_URL || ''
 	const api = `${baseApiUrl}/auth/login`
 
-const handleSubmit = async (e: React.FormEvent) => {
-  e.preventDefault()
-  try {
-    const response = await fetch(api, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ userEmail, password }),
-    })
-    if (!response.ok) {
-      const errorMessage = await response.text()
-      throw new Error(`Sign in failed: ${errorMessage || response.statusText}`)
-    }
-    const data = await response.json()
-    if (!data.token || !data.user) {
-      throw new Error("Invalid response format. Missing token or user data.")
-    }
+	const handleSubmit = async (e: React.FormEvent) => {
+		e.preventDefault()
+		if (!userEmail || !password) {
+			alert('Please enter your email and password')
+			return
+		}
 
-    login(data.token, data.user)
-    router.push(`/dashboard`)
+		setIsLoading(true)
+		try {
+			const response = await fetch(api, {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({ userEmail, password }),
+			})
+			if (!response.ok) {
+				const errorMessage = await response.text()
+				throw new Error(`Sign in failed: ${errorMessage || response.statusText}`)
+			}
+			const data = await response.json()
+			if (!data.token || !data.user) {
+				throw new Error("Invalid response format. Missing token or user data.")
+			}
 
-  } catch (error) {
-    console.error('An error occurred:', error)
-    alert(`Login failed: ${error instanceof Error ? error.message : 'Unknown error'}`)
-  }
-}
+			login(data.token, data.user)
+			setIsLoading(false)
+			router.push(`/dashboard`)
+
+		} catch (error) {
+			console.error('An error occurred:', error)
+			alert(`Login failed: ${error instanceof Error ? error.message : 'Unknown error'}`)
+		}
+	}
+
+	if (isLoading) {
+		return <div className="flex justify-center items-center h-screen">Connecting to server...</div>
+	}
 
 	return (
 		<div className="w-full max-w-md mx-auto mt-10 bg-white shadow-md rounded-lg overflow-hidden">
